72ac368c7d4d2bd3973bd5b27518e9d56b908b3c
Lorena-lisseth <2431186@clases.edu.sv>
2024-02-21 10:00:25 -0600

    Agregar archivos de proyecto.

---
+++ PruebaTec02LLVG/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaTec02LLVG.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
